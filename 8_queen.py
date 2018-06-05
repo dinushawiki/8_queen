89b7151d6871f2dfc660134865ce158e36c2a47b
dinushawiki <dinushawiki@gmail.com>
2018-06-05 03:01:55 -0400

    fixing an error in algorithm

--- 8_queen.py
+++ 8_queen.py
@@ -6,13 +6,13 @@ class EightQueen:
         self.board = board
         self.complete = True
 
-    def get_h_cost(self):
+    def get_h_cost(self,problem):
         h = 0
-        for i in range(len(board)):
+        for i in range(len(problem)):
             #Check every column we haven't already checked
-            for j in range(i + 1,len(board)):
+            for j in range(i + 1,len(problem)):
                 #Queens are in the same row
-                if board[i] == board[j]:
+                if problem[i] == problem[j]:
                     h += 1
 
                 #Get the difference between the current column
@@ -20,29 +20,29 @@ class EightQueen:
                 offset = j - i
                 #To be a diagonal, the check column value has to be 
                 #equal to the current column value +/- the offset
-                if board[i] == board[j] - offset or board[i] == board[j] + offset:
+                if problem[i] == problem[j] - offset or problem[i] == problem[j] + offset:
                     h += 1
         return h
 
     def make_move_steepest_hill(self):
-        board = self.board
-        h_to_beat = EightQueen.get_h_cost(board)
+        h_to_beat = EightQueen.get_h_cost(self, self.board)
+        print(h_to_beat)
         if h_to_beat == 0:
             self.complete = False
-            return board
+            return self.board
         moves = {}
-        for col in range(len(board)):
+        for col in range(len(self.board)):
 
-            for row in range(len(board)):
-                if board[col] == row:
+            for row in range(len(self.board)):
+                if self.board[col] == row:
                     #We don't need to evaluate the current
                     #position, we already know the h-value
                     continue
         
-                board_copy = list(board)
+                board_copy = list(self.board)
                 #Move the queen to the new row
                 board_copy[col] = row
-                moves[(col,row)] = EightQueen.get_h_cost(board_copy)
+                moves[(col,row)] = EightQueen.get_h_cost(self,board_copy)
         best_moves = []
         for move,h_cost in moves.items():
             if h_cost < h_to_beat:
@@ -70,7 +70,7 @@ class EightQueen:
             print(move)
 
 if __name__ == "__main__":
-    board = [0,0,1,2,4,3]
+    board = [0,0,1,2,4,0,1,2]
     print("Problem")
     print(board)
     out = EightQueen(board)
